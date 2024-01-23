71032ee6101c50f7f6215d8112de22d4e931bc20
taoliujun <taoliujun@qq.com>
2024-01-23 16:58:52 +0800

    docs(changeset): feat: beta1

---
+++ .changeset/hip-zoos-agree.md
@@ -0,0 +1,5 @@
+---
+'svg-to-component.parse': minor
+---
+
+feat: beta1
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,10 +1,11 @@
 import { Command } from 'commander';
 import { commandParse } from './parse';
 import { commandPreview } from './preview';
+import pkg from '../package.json';
 
 const program = new Command();
 
-program.name('svg-to-component').description('svg file parse and preview.').version('1.0.0');
+program.name('svg-to-component').description('svg file parse and preview.').version(pkg.version);
 program.addCommand(commandParse);
 program.addCommand(commandPreview);
 
