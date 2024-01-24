7441522b8397a58519c09fe1764ef8d24e616e12
taoliujun <taoliujun@qq.com>
2024-01-24 17:34:44 +0800

    fix: update

--- svg-component-output/utils/types.ts
+++
@@ -1,7 +0,0 @@
-import type { SVGAttributes } from 'react';
-
-interface SVGIconProps extends Omit<SVGAttributes<SVGElement>, 'color'> {
-    color?: string | string[];
-}
-
-export type { SVGIconProps };
